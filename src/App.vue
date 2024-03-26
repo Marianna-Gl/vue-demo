47a7661ca19000d04f55cfe823e51b9a9f915f54
Marianna Galichyan <marianna.galichyan@gmail.com>
2024-03-26 12:36:49 +0400

    added functions

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import {ref} from 'vue'
 let count= ref(0);
-let secondCount=ref(0);
+
 function increment(){
 count.value++;
 }
