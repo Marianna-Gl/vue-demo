f680b4d4224cd1799c776745aedbe567ff3c98f4
Marianna Galichyan <marianna.galichyan@gmail.com>
2024-03-29 00:05:30 +0400

    code created

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <script>
-
+export default {
+  data() {
+    return {
+      email: '',
+      isValid: false
+    };
+  },
+  methods: {
+    openPrompt() {
+      const userInput = prompt('Please enter your email');
+      if (userInput !== null) {
+        this.email = userInput;
+        this.isValid = this.validateEmail(userInput);
+      }
+    },
+    reset() {
+      this.email = '';
+      this.isValid = false;
+    },
+    validateEmail(email) {
+      // Regular expression for basic email validation
+      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return regex.test(email);
+    }
+  }
+};
 </script>
 
+
 <template>
+  <div>
+    <button @click="openPrompt">Input Email</button>
+    <button @click="reset">Reset</button>
 
-
-  <main>
->
-  </main>
+    <div v-if="email">
+      <h1 :style="{borderColor: isValid ? 'green' : 'red'}">{{ email }}</h1>
+      <h4>{{ isValid ? 'The email is valid' : 'The email is invalid' }}</h4>
+    </div>
+  </div>
 </template>
 
-<style scoped>
 
+
+<style>
+h1 {
+  border: 2px solid;
+  padding: 10px;
+  display: inline-block;
+}
 </style>
