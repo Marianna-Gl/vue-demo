057faeae4102e0ed8f39fe853094187f8a6955d9
Marianna Galichyan <marianna.galichyan@gmail.com>
2024-03-31 01:15:26 +0400

    new changes

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,19 @@
 <script>
+import Exchange from './components/Exchange.vue'
 export default {
-  data() {
-    return {
-      email: '',
-      isValid: false
-    };
+  components: {
+    Exchange
   },
-  methods: {
-    openPrompt() {
-      const userInput = prompt('Please enter your email');
-      if (userInput !== null) {
-        this.email = userInput;
-        this.isValid = this.validateEmail(userInput);
-      }
-    },
-    reset() {
-      this.email = '';
-      this.isValid = false;
-    },
-    validateEmail(email) {
-      // Regular expression for basic email validation
-      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return regex.test(email);
-    }
-  }
-};
+  data() {
+    return {}
+  },
+  methods: {}
+}
 </script>
 
-
 <template>
-  <div>
-    <button @click="openPrompt">Input Email</button>
-    <button @click="reset">Reset</button>
-
-    <div v-if="email">
-      <h1 :style="{borderColor: isValid ? 'green' : 'red'}">{{ email }}</h1>
-      <h4>{{ isValid ? 'The email is valid' : 'The email is invalid' }}</h4>
-    </div>
-  </div>
+  <Exchange />
+  <div></div>
 </template>
 
-
-
-<style>
-h1 {
-  border: 2px solid;
-  padding: 10px;
-  display: inline-block;
-}
-</style>
+<style scoped></style>
---
+++ src/components/Exchange.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data() {
+    return {}
+  }
+//   methods:{
+//     typeCurrency(event){
+//         console.log(event.target.value);
+//     }
+
+
+//     }
+  };
+
+</script>
+
+<template>
+  {{ Exchange }}
+  <div>AMD -> USD կամ USD -> AMD</div>
+  <br />
+  <div>
+    <input type="text" @input="typeCurrency" />
+    <input type="text" @input="typeCurrency" />
+  </div>
+  <br /><br />
+  <button>AMD -> USD կամ USD -> AMD</button>
+</template>
+
+<style scoped></style>
