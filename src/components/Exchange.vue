4940e0778c08c1e76e7f153623541e892368abe6
Marianna Galichyan <marianna.galichyan@gmail.com>
2024-04-01 21:42:35 +0400

    new changes

--- src/components/Exchange.vue
+++ src/components/Exchange.vue
@@ -2,44 +2,46 @@
 export default {
   data() {
     return {
-      usd,
-      amd
+      mode: 'AMD', // Initial mode
+      amount: '' // Input value
+    }
+  },
+  methods: {
+    toggleMode() {
+      this.mode = this.mode === 'AMD' ? 'USD' : 'AMD'
     }
   }
 }
-//   methods:{
-//     typeCurrency(event){
-//         console.log(event.target.value);
-//     }
-
-//     }
 </script>
 
 <template>
   {{ Exchange }}
 
-  <div>AMD -> USD կամ USD -> AMD</div>
-  <br />
   <div>
-    <input type="text" @input="typeCurrency" />
-    <input type="text" @input="typeCurrency" />
+    <h2>{{ mode === 'AMD' ? 'AMD -> USD' : 'USD -> AMD' }}</h2>
+    <div>
+      <input type="text" @input="typeCurrency" />
+      <input type="text" @input="typeCurrency" />
+    </div>
+    <!-- <input type="text" v-model="amount" /> -->
+    <button @click="toggleMode">{{ mode !== 'AMD' ? 'AMD -> USD' : 'USD -> AMD' }}</button>
   </div>
-  <br /><br />
-  <button @click="chooseCurrecncy">
-    <!-- Display the current conversion mode -->
-    <p v-if="mode === 'amd-to-usd'">Conversion Mode: AMD to USD</p>
-    <p v-else>Conversion Mode: USD to AMD</p>
 
-    <!-- Display the conversion component based on the mode -->
-    <div v-if="mode === 'amd-to-usd'">
-      <p>Convert AMD to USD</p>
-      <!-- Your conversion component for AMD to USD -->
-    </div>
+  <!-- Display the current conversion mode -->
+  <!-- <p v-if="mode === 'amd-to-usd'">Conversion Mode: AMD to USD</p>
+    <p v-else>Conversion Mode: USD to AMD</p> -->
+
+  <!-- Display the conversion component based on the mode -->
+  <!-- <div v-if="mode === 'amd-to-usd'">
+      <p>Convert AMD to USD</p> -->
+  <!-- Your conversion component for AMD to USD -->
+  <!-- </div>
     <div v-else>
-      <p>Convert USD to AMD</p>
-      <!-- Your conversion component for USD to AMD -->
-    </div>
+      <p>Convert USD to AMD</p> -->
+  <!-- Your conversion component for USD to AMD -->
+  <!-- </div>
   </button>
+  -->
 </template>
 
 <style scoped></style>
