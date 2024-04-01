aaa78e20dae318f8c5b90f1890110506306d4a7b
Marianna Galichyan <marianna.galichyan@gmail.com>
2024-04-01 19:04:55 +0400

    new modifications

--- src/components/Exchange.vue
+++ src/components/Exchange.vue
@@ -1,21 +1,23 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      usd,
+      amd
+    }
   }
+}
 //   methods:{
 //     typeCurrency(event){
 //         console.log(event.target.value);
 //     }
 
-
 //     }
-  };
-
 </script>
 
 <template>
   {{ Exchange }}
+
   <div>AMD -> USD կամ USD -> AMD</div>
   <br />
   <div>
@@ -23,7 +25,21 @@ export default {
     <input type="text" @input="typeCurrency" />
   </div>
   <br /><br />
-  <button>AMD -> USD կամ USD -> AMD</button>
+  <button @click="chooseCurrecncy">
+    <!-- Display the current conversion mode -->
+    <p v-if="mode === 'amd-to-usd'">Conversion Mode: AMD to USD</p>
+    <p v-else>Conversion Mode: USD to AMD</p>
+
+    <!-- Display the conversion component based on the mode -->
+    <div v-if="mode === 'amd-to-usd'">
+      <p>Convert AMD to USD</p>
+      <!-- Your conversion component for AMD to USD -->
+    </div>
+    <div v-else>
+      <p>Convert USD to AMD</p>
+      <!-- Your conversion component for USD to AMD -->
+    </div>
+  </button>
 </template>
 
 <style scoped></style>
